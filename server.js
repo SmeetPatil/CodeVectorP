07f1694774dd690ebb2e76c7cf6a53930de59f51
Smeet Patil <smeetpatil878@gmail.com>
2026-06-24 00:58:32 +0530

    Commit #4 - Bugfix

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const { getProductsPage } = require('./products');
 const { encodeCursor, decodeCursor } = require('./cursor');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,(('public')))));
 
 const DEFAULT_LIMIT = 20;
 const MAX_LIMIT = 100;
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
